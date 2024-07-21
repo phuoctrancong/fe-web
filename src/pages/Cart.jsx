8cfdf4dbcbfc7fab050bd9ab14bba767d4e8ff08
phuoctrancong <phuoc.trancong@vti.com.vn>
2024-07-21 09:44:13 +0700

    update code

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -32,6 +32,7 @@ const Cart = () => {
         0
       )
     );
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [getFromLocal("cart")]);
   return (
     <Helmet title="Giỏ hàng">
